796209c3f66aeceda5f58a7a4cc200010f3f7b1a
Vladislav Nikolov <vladis890@gmail.com>
2018-11-27 18:23:19 +0100

    Arreglado trigger Empleado.

--- sql/TriggerRS1.2.sql
+++ sql/TriggerRS1.2.sql
@@ -1,9 +1,16 @@
-USE pomodoro;
+USE pomodoro
 
-delimiter //
+-- Gestión de Empleados
+
+DELIMITER |
 CREATE TRIGGER eliminar_emp BEFORE DELETE ON Empleado
 	FOR EACH ROW
 	BEGIN
-		DELETE FROM asignacion WHERE asignacion.dni_emp = OLD.dni_emp
-	END; //
-delimiter;
+		DELETE FROM asignacion WHERE asignacion.dni_emp = OLD.dni_emp;
+	END
+	|
+DELIMITER ;
+
+-- Gestión de Menús
+
+
