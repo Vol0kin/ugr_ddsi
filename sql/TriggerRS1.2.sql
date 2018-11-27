ba4c0099de068260914d3fdd078dc9803c30f380
Nazaret <nazaretrogue@correo.ugr.es>
2018-11-27 18:24:47 +0100

    Trigger proveedores añadido

--- sql/TriggerRS1.2.sql
+++ sql/TriggerRS1.2.sql
@@ -13,4 +13,24 @@ DELIMITER ;
 
 -- Gestión de Menús
 
+-- Gestión de Proveedores
+
+DELIMITER |
+
+CREATE TRIGGER disp_proveedores 
+BEFORE INSERT ON Proveedor
+BEGIN
+	IF NEW.edad < 18 THEN
+		SIGNAL SQLSTATE '55555'
+		SET MESSAGE_TEXT = 'Error en la edad del proveedor. No puede ser menor que 18';
+
+	ELSE
+		INSERT NEW INTO Proveedor;
+	END IF;
+END;
+
+|
+
+DELIMITER ;
+
 
